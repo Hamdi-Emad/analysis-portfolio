e6c52ff7e18b2ebb76803c9772c336e138b81237
Hamdi Emad <hamdi2002em@gmail.com>
2025-07-29 18:07:14 +0300

    update about

--- src/app/_components/about.jsx
+++ src/app/_components/about.jsx
@@ -4,7 +4,6 @@ import { TbBrandGithubFilled } from "react-icons/tb";
 import { MdOutlineMailOutline } from "react-icons/md";
 import { IoCall } from "react-icons/io5";
 
-
 export default function About() {
    return (
       <>
@@ -36,12 +35,51 @@ export default function About() {
                      </p>
                   </div>
                   <div className="flex flex-col md:flex-row justify-center items-center gap-10 mt-10  w-full">
+                     <div className="icons flex justify-center items-center gap-5 ">
+                        <div className="cursor-pointer bg-[#0a66c2] rounded-full hover:bg-white hover:text-[#0a66c2] transition hover:scale-[1.1] hover:rotate-[360deg] duration-[0.5s] ">
+                           <a
+                              href="https://www.linkedin.com/in/hamdi-emad1/"
+                              target="_blank"
+                              className="p-3 block"
+                           >
+                              <FaLinkedinIn size={22} />
+                           </a>
+                        </div>
+                        <div className="cursor-pointer bg-[#333333] rounded-full hover:bg-white hover:text-[#333333] transition hover:scale-[1.1] hover:rotate-[360deg] duration-[0.5s] ">
+                           <a
+                              href="https://github.com/Hamdi-Emad"
+                              target="_blank"
+                              className="p-3 block"
+                           >
+                              <TbBrandGithubFilled size={22} />
+                           </a>
+                        </div>
+                        <div className="cursor-pointer bg-[#1877f2]  rounded-full hover:bg-white hover:text-[#1877f2] transition hover:scale-[1.1] hover:rotate-[360deg] duration-[0.5s] ">
+                           <a
+                              href="https://www.facebook.com/hamdi.emad.509/"
+                              target="_blank"
+                              className="p-3 block"
+                           >
+                              <FaFacebookF size={22} />
+                           </a>
+                        </div>
+                        <div className="cursor-pointer bg-[#1877f2]  rounded-full hover:bg-white hover:text-[#1877f2] transition hover:scale-[1.1] hover:rotate-[360deg] duration-[0.5s] ">
+                           <a
+                              href="tel:201212384862"
+                              target="_blank"
+                              className="p-3 block"
+                           >
+                              <IoCall size={22} />
+                           </a>
+                        </div>
+                     </div>
                      <div className="buttons flex justify-center items-center gap-5 ">
                         <button className="bg-[#00a8cd] w-[60%] md:w-auto cursor-pointer hover:bg-[#009dcde8] transition text-[16px]  rounded-[5px] ">
                            <a
                               href="mailto:hamdi2002em@mail.com"
                               // href="https://mail.google.com/mail/?view=cm&fs=1&to=hamdi2002em@mail.com"
-                              target="_blank" rel="noopener noreferrer"
+                              target="_blank"
+                              rel="noopener noreferrer"
                               className="flex justify-between gap-2 items-center py-3 px-4"
                            >
                               <span>
@@ -63,44 +101,6 @@ export default function About() {
                               <span className="text-nowrap">WhatsApp Me</span>
                            </a>
                         </button>
-                     </div>
-                     <div className="icons flex justify-center items-center gap-5 ">
-                        <div className="cursor-pointer bg-[#0a66c2] rounded-full hover:bg-white hover:text-[#0a66c2] transition">
-                           <a
-                              href="https://www.linkedin.com/in/hamdi-emad1/"
-                              target="_blank"
-                              className="p-3 block"
-                           >
-                              <FaLinkedinIn size={22} />
-                           </a>
-                        </div>
-                        <div className="cursor-pointer bg-[#333333] rounded-full hover:bg-white hover:text-[#333333] transition">
-                           <a
-                              href="https://github.com/Hamdi-Emad"
-                              target="_blank"
-                              className="p-3 block"
-                           >
-                              <TbBrandGithubFilled size={22} />
-                           </a>
-                        </div>
-                        <div className="cursor-pointer bg-[#1877f2]  rounded-full hover:bg-white hover:text-[#1877f2] transition">
-                           <a
-                              href="https://www.facebook.com/hamdi.emad.509/"
-                              target="_blank"
-                              className="p-3 block"
-                           >
-                              <FaFacebookF size={22} />
-                           </a>
-                        </div>
-                        <div className="cursor-pointer bg-[#1877f2]  rounded-full hover:bg-white hover:text-[#1877f2] transition">
-                           <a
-                              href="tel:201212384862"
-                              target="_blank"
-                              className="p-3 block"
-                           >
-                              <IoCall size={22}/>
-                           </a>
-                        </div>
                      </div>
                   </div>
                </div>
