91c8c2d4e20c55c70f1c98ddc17e5641643d87a5
Hamdi Emad <hamdi2002em@gmail.com>
2025-08-02 02:36:55 +0300

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -16,11 +16,57 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-   title: "Hamdi Emad's Portfolio",
-   description:
-      "Hamdi Emad is a Data Analyst who turns raw data into actionable insights using Excel, SQL, Python, and Power BI. Explore data-driven projects, dashboards, and analytical skills.",
+   title: "Hamdi Emad | Data Analyst Portfolio",
    icons: {
-      icon: "/favicon.ico", // لو اسم الصورة favicon.ico بدلها هنا
+      icon: "/favicon.ico",
+   },
+   description:
+      "I'm Hamdi Emad, a data analyst skilled in Excel, SQL, Python, and Power BI. View my data analysis portfolio including dashboards, visualizations, and real-world insights.",
+   keywords: [
+      "Data Analyst",
+      "Data Analysis Portfolio",
+      "Data Analytics Projects",
+      "Business Intelligence",
+      "Data Visualization",
+      "Excel",
+      "SQL",
+      "Python",
+      "Pandas",
+      "Power BI",
+      "Tableau",
+      "Data Cleaning",
+      "Data Wrangling",
+      "Data Interpretation",
+      "Data Storytelling",
+      "Dashboard Developer",
+      "Data Enthusiast",
+      "Freelance Data Analyst",
+      "Junior Data Analyst",
+      "Entry Level Data Analyst",
+      "Data Insights",
+      "Hamdi Emad",
+      "Hamdi Emad Portfolio",
+      "Data Analyst Egypt",
+   ],
+   authors: [
+      { name: "Hamdi Emad", url: "https://analysis-portfolio.vercel.app/" },
+   ],
+   openGraph: {
+      title: "Hamdi Emad | Data Analyst Portfolio",
+      description:
+         "Explore real-world data analysis projects by Hamdi Emad. Specializing in Excel, SQL, Python, Pandas, Power BI, and data visualization. Turning raw data into meaningful insights.",
+      url: "https://analysis-portfolio.vercel.app",
+      siteName: "Hamdi Emad | Data Analyst",
+      images: [
+         {
+            url: "https://raw.githubusercontent.com/Hamdi-Emad/analysis-portfolio/refs/heads/main/public/logo.png",
+            width: 1200,
+            height: 630,
+            alt: "Dashboard and Data Analysis Projects by Hamdi Emad",
+         },
+      ],
+      locale: "en_US",
+      type: "website",
    },
 };
 
@@ -31,6 +77,10 @@ export default function RootLayout({ children }) {
             <link
                rel="stylesheet"
                href="https://unpkg.com/aos@next/dist/aos.css"
+            />
+            <meta
+               name="google-site-verification"
+               content="qJvhc4dQCZms9Rf3LO9J1c6HO3oSn2Dd-cLgQqRvjug"
             />
          </head>
          <body
