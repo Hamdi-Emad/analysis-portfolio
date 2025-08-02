1fdae9276d9f34f014a6e6443ec6d1e3e7754662
Hamdi Emad <hamdi2002em@gmail.com>
2025-08-02 03:23:17 +0300

    update

---
+++ src/app/projects/page.jsx
@@ -0,0 +1,74 @@
+import ProjectCard from "../_components/projectCard/projectCard";
+
+export default async function Projects() {
+   const response = await fetch(
+      "https://raw.githubusercontent.com/Hamdi-Emad/analysis-api/refs/heads/main/projects.json",
+      { cache: "no-store" }
+   );
+   const data = await response.json();
+   return (
+      <>
+         <div className="home-holder">
+            <div
+               className="container mx-auto px-5 pb-10 md:px-10 2xl:px-7"
+               id="home"
+            >
+               <div className="pt-40 " id="projects">
+                  <div>
+                     <div className="sec-title  text-center pb-10">
+                        <h2>Projects</h2>
+                     </div>
+                     <div>
+                        {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 "> */}
+                        <ProjectCard projectData={data} />
+
+                        {data.map((project, i) => (
+                           // <div
+                           //    key={i}
+                           //    className="skill-card p-[20px] shadow-lg group overflow-hidden"
+                           // >
+                           //    <div className=" w-full relative h-[300px] overflow-hidden rounded-[12px]">
+                           //       <a
+                           //          href={project.link}
+                           //          className="block"
+                           //          target="_blank"
+                           //          rel="noopener noreferrer"
+                           //       >
+                           //          <Slider images={project.images} />
+                           //       </a>
+                           //       {/* <Image
+                           //          priority
+                           //          src={project.image}
+                           //          fill
+                           //          alt={project.title}
+                           //          quality={100}
+                           //          className="group-hover:scale-[1.1]  transition-all duration-[0.4s] rounded-[12px]"
+                           //       /> */}
+                           //    </div>
+                           //    <div className="">
+                           //       <h3 className="text-center pt-8 pb-6 px-4 text-2xl text-[#00e1ffe8] font-[700]">
+                           //          {project.title}
+                           //       </h3>
+                           //       <p className="leading-[1.6] text-center px-6">
+                           //          {project.description}
+                           //       </p>
+                           //    </div>
+                           //    <div className="flex flex-wrap justify-center gap-2 pt-6">
+                           //       {project.tools.map((tool, i) => (
+                           //          <p className="border-1 border-[#00e1ffe8] rounded-[10px] px-3 py-[3px] text-nowrap" key={i}>{tool}</p>
+                           //       ))}
+                           //    </div>
+                           // </div>
+                           <p className="pt-96 hidden" key={i}>
+                              {project.title}
+                           </p>
+                        ))}
+                        {/* </div> */}
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </>
+   );
+}
---
+++ src/app/skills/page.jsx
@@ -0,0 +1,75 @@
+import Image from "next/image";
+import { MdKeyboardDoubleArrowRight } from "react-icons/md";
+
+export default async function Skills() {
+   const response = await fetch(
+      "https://raw.githubusercontent.com/Hamdi-Emad/analysis-api/refs/heads/main/skills.json",
+      { cache: "no-store" }
+   );
+   const data = await response.json();
+   return (
+      <>
+         <div className="home-holder">
+            <div
+               className="container mx-auto px-5 pb-10 md:px-10 2xl:px-7"
+               id="home"
+            >
+               <div className="pt-40" id="skills">
+                  <div>
+                     <div className="sec-title  text-center pb-10">
+                        <h2>Skills</h2>
+                     </div>
+                     <div>
+                        {/* //* انا زودت درجة في الاتنين ميديا لو غيرت ابقي شيلهم */}
+                        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3  gap-5 ">
+                           {data.map((skill, i) => (
+                              <div
+                                 key={i}
+                                 className="skill-card px-5 shadow-lg group "
+                              >
+                                 <div className="w-full h-40  flex justify-center items-center">
+                                    <Image
+                                       src={skill.image}
+                                       width={100}
+                                       height={100}
+                                       alt={skill.name}
+                                       className="group-hover:scale-[1.2] transition-all duration-[0.4s] "
+                                    />
+                                 </div>
+                                 <div>
+                                    <h3 className="text-center py-3 text-2xl text-[#0ab8ece8] font-[700]">
+                                       {skill.name}
+                                    </h3>
+                                    <div>
+                                       <ul className="">
+                                          {skill.details.map((item, i) => (
+                                             <li
+                                                className="flex items-start gap-1 py-3 my-2 hover:pl-2 md:hover:pl-3 transition-all hover:bg-[rgb(0,0,0,0.2)] rounded-lg duration-[0.3s] cursor-pointer"
+                                                key={i + 100}
+                                             >
+                                                <span className=" shrink-0  md:pt-[0.5px]">
+                                                   <MdKeyboardDoubleArrowRight
+                                                      size={25}
+                                                      color="#009dcde8"
+                                                      className=""
+                                                   />
+                                                </span>
+                                                <span className="pt-[1px] md:pt-0 text-[15px] md:text-[17px] leading-[1.6]">
+                                                   {item}
+                                                </span>
+                                             </li>
+                                          ))}
+                                       </ul>
+                                    </div>
+                                 </div>
+                              </div>
+                           ))}
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </>
+   );
+}
